ff40839883999f9a7a842f2ea6518926389ad081
huxiaofei1689 <1344780209@qq.com>
2019-07-16 08:54:08 +0800

    9天日考

---
+++ 9.php
@@ -0,0 +1,16 @@
+<?php
+$number = [3,32,321];
+$number = PrintMinNumber($numbers);
+echo $number;
+function PrintMinNumber($numbers){
+    $data = [];
+    for($i=1,$i<3,$i++){
+        for($j=1,$j<3,$j++){
+            for($k=1,$k<3,$k++){
+
+            }
+        }
+    }
+    $numbers[$i+1]=!$numbers[$j+1] && $numbers[$k+1]=!$numbers[$i+1] && $numbers[$j+1]=!$numbers[$k+1];
+}
+echo $numbers;
