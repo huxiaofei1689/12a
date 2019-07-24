40b308193635b5d76b95692dc2584a2a6d86f6f9
huxiaofei1689 <1344780209@qq.com>
2019-07-24 08:47:48 +0800

    16日考

---
+++ 16.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 得到
+ * Date: 2019/7/24
+ * Time: 8:34
+ */
+ public function day(){
+    $target = 7;
+     $array=[[1,2,8,9],[2,4,9,12],[4,7,10,13],[6,8,11,15]];
+
+    if($this->Find($target,$array)){
+        echo "存在";
+    }else{
+        echo "不存在";
+    }
+}
