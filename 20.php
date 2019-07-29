91e48e318561e27386b98087bd9f7cab0012bbbd
huxiaofei1689 <1344780209@qq.com>
2019-07-29 08:46:37 +0800

    20日考

---
+++ 20.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 得到
+ * Date: 2019/7/29
+ * Time: 8:40
+ */
+echo CountSteps($x,$y);
+
+function day20($x,$y){
+    echo $x,$y;
+    $array = $x=1,$y=1l;
+}
